52e5a760bf737ce6ccc24519a842f12edc5eaf82
balagrivine <balagrivine@gmail.com>
2023-10-04 09:33:34 -0700

    write a python program that implements the fizzbuzz classical programming question

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
+
     num = 1
     while num <= 100:
         sep = ' ' if num <= 99 else '\n'
         if (num % 3 == 0) and (num % 5 == 0):
-            print("FizzBuzz", end = sep)
+            print("FizzBuzz", end=sep)
         elif num % 3 == 0:
-            print("Fizz", end = sep)
+            print("Fizz", end=sep)
         elif num % 5 == 0:
-            print("Buzz", end = sep)
+            print("Buzz", end=sep)
         else:
-            print("{}" .format(num), end = sep)
+            print(num, end=sep)
         num += 1
+
