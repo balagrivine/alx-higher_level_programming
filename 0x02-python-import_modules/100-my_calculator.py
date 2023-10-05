5967a4697b78cd51f2f21d9ca583003b83825ba5
balagrivine <balagrivine@gmail.com>
2023-10-05 09:17:49 -0700

    write a program that imorts all functions from a file and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -17,8 +17,8 @@ if __name__ == "__main__":
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            op = sys.argv[2]
-            result = operators[op](a, b)
-            print("{} {} {} = {}" .format(a, sys.argv[2], b, result))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    result = operators[op](a, b)
+    print("{} {} {} = {}" .format(a, sys.argv[2], b, result))
