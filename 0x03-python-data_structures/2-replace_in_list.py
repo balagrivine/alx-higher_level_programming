9559139422189cb86b77a422608aa6d65f6a9863
balagrivine <balagrivine@gmail.com>
2023-10-07 03:23:14 -0700

    write a function that replaces an element of a list at  aspecific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,5 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     my_list[idx] = element
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
 
     return (None if idx < 0 and idx > len(my_list) else my_list)
