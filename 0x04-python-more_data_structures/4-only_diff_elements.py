00a92cd44cc2d6d9217df081b1911846db38508a
balagrivine <balagrivine@gmail.com>
2023-10-11 12:37:03 -0700

    write a fnction that returns unique elements in two sets

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    my_set = set()
-    for x in set_1:
-        for y in set_2:
-            if x not in set_2 and y not in set_1:
-                my_set.add(x)
-                my_set.add(y)
-    return my_set
+    return (set_1 ^ set_2)
