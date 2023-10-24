b4273c22a65c265c8679cff9727560182ea859d1
balagrivine <balagrivine@gmail.com>
2023-10-24 12:46:19 -0700

    write a class square that defines a square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Square():
+    """Initialize a class"""
+    def __init__(self, size=0):
+
+        """
+            Initialize a new square
+
+        Args:
+            size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size mus be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
