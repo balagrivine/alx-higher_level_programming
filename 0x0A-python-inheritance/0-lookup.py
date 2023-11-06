4c4f247ec32823218139b36699ddeda630c03cf8
balagrivine <balagrivine@gmail.com>
2023-11-06 10:38:24 -0800

    write a function that returns list of availabe attributes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-
+"""A function to rturn list of variables"""
 def lookup(obj):
     """Function that returns a list of available attributes and methods"""
     return dir(obj)
