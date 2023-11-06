a3455b28e41b87581518a4444c7268d3543066b1
balagrivine <balagrivine@gmail.com>
2023-11-06 14:18:45 -0800

    checks for equality or inequality

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""python class that inverts == and !=
+"""
+
+class MyInt(int):
+    """inherits from int
+
+    Args:
+        int: python object
+    """
+    def __eq__(self, value):
+        """equal to command
+
+        Args:
+        value: value to be compared
+        """
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """not-equal method
+
+        Args:
+            value: value to be compared
+
+        """
+        return super().__eq__(value)
