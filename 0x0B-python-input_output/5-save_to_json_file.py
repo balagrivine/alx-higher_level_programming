80e1b0b9b14799b5fc656146d229ccfe106752ae
balagrivine <balagrivine@gmail.com>
2023-11-07 09:19:10 -0800

    write a function that write JSON str rep to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+"""write function"""
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an object to a txt file
+
+    Args:
+        my_obj: object
+        filename: file
+    """
+
+    with open(filename, "w", encoding="utf-8") as file:
+        obj = json.dumps(my_obj)
+        file.write(obj)
