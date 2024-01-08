8dd67b8422459d9e34b99e3c06b3149b0ee95f32
balagrivine <balagrivine@gmail.com>
2024-01-08 06:38:05 -0800

    write a script that prints x times a text

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let i = 0;
+
+if (isNaN(process.argv[2]))
+{
+	console.log('Missing number of occurrences');
+}
+else
+{
+	while (i < process.argv[2])
+	{
+		console.log('C is fun');
+		i++;
+	}
+}
