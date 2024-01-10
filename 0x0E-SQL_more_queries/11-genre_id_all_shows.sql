217ae8045c8f2028a2f1e470871f34008d40a9d3
balagrivine <balagrivine@gmail.com>
2024-01-10 05:51:30 -0800

    write a script that imports a database and lists al shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -4,5 +4,5 @@
 -- If a show doesn’t have a genre, display NULL
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.genre_id 
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id 
 ORDER BY tv_shows.title, tv_show_genre_genres.genre_id;
