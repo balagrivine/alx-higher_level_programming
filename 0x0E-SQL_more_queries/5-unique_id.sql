54d63f37f89304bf1c50783625ff636fea400692
balagrivine <balagrivine@gmail.com>
2024-01-10 02:16:46 -0800

    write a script that creates a table and sets constraints to unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- create a table that with values set to unique
 
-CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, name VARCHAR(256), UNIQUE(id));
