dbf12a3efa9cd662dc517d82d5aa06199ea69965
balagrivine <balagrivine@gmail.com>
2024-01-10 02:01:25 -0800

    write a script that creates a database and user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- a script that creates a database and user
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT to 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
