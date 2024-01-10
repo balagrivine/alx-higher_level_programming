bde76e6a997ce32b08db43cacfbda6a979bad8fe
balagrivine <balagrivine@gmail.com>
2024-01-10 03:31:49 -0800

    write a script that imports a database and lists al shows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,6 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_shows_genre.genre_id 
-FROM hbtn_0d_tvshows
-ORDER BY tv_shows.title AND tv_show_genres.genre_id ASC;
+FROM tv_shows
+tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
