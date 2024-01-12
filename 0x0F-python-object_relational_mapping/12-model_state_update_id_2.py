c6f6e5c422f7c1a28e629a4efbb9a185459646ef
balagrivine <balagrivine@gmail.com>
2024-01-12 11:43:20 -0800

    Write a script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 
 
-"""
-Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""Write a script that changes the name of a State object from the database hbtn_0e_6_usa
 
 Your script should take 3 arguments: mysql username, mysql password and database name
 You must use the module SQLAlchemy
 You must import State and Base from model_state - from model_state import Base, State
 Your script should connect to a MySQL server running on localhost at port 3306
-Print the new states.id after creation
+Change the name of the State where id = 2 to New Mexico
 Your code should not be executed when imported
 
 """
 import sqlalchemy
-from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
-import sys
+from sys import argv
+from sqlalchemy.orm import declarative_base, sessionmaker
 from model_state import State, Base
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = State(name="Louisiana")
-    session.add(state)
+    state_update = session.query(State).filter(State.id==2).first()
+
+    state_update.name = "New Mexico"
+
     session.commit()
-
-    print("{}".format(state.id))
-
     session.close()
