a1ffed8f6fd490814f955ec98aebb18900db3b91
balagrivine <balagrivine@gmail.com>
2024-01-14 06:02:34 -0800

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 
-import MySQLdb
-import sys
 
 """
 Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
@@ -16,12 +14,14 @@ Results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+if __name__ == "__main__":
+    import MySQLdb, sys
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-conn = db.cursor()
-conn.execute("SELECT DISTINCT name, id FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
+    conn = db.cursor()
+    conn.execute("SELECT * FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
 
-[print(states) for states in conn.fetchall()]
+    [print(states) for states in conn.fetchall()]
 
-db.close()
-conn.close()
+    conn.close()
+    db.close()
