7827831a34fde05933a4a1aae2fd730f24ef939e
balagrivine <balagrivine@gmail.com>
2024-01-14 05:36:37 -0800

    Write a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,8 +16,7 @@ Your code should not be executed when imported
 """
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3], host='localhost', port=3306)
     conn = db.cursor()
 
     query = "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id=cities.state_id ORDER BY cities.id "
