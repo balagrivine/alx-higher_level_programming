e64ac10932f6384a67d0bd916d330dbd364c187f
balagrivine <balagrivine@gmail.com>
2024-01-14 06:09:09 -0800

    Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 
-import MySQLdb
-from sys import argv
 
 """
 Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa
@@ -17,6 +15,8 @@ Your code should not be executed when imported
 """
 
 if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 
