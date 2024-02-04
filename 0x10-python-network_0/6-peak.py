e35d8a46f427911cf9b2663206fb76e35187e6e8
balagrivine <balagrivine@gmail.com>
2024-02-04 09:03:03 -0800

    write a function that fins a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that finds a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """A  function to find a peak in a list of unsorted integer"""
+    max_int = max(list_of_integers)
