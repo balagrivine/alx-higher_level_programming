35ff4dabe7729d956ce41fb7aab0dc55cc6a9ad6
balagrivine <balagrivine@gmail.com>
2024-02-04 05:37:06 -0800

    Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-email="test@gmail.com"
-subject="I will always be here for PLD"
-curl -sLX  POST $1 -d "$email" -d "$subject"
+curl -sLX  POST $1 -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
