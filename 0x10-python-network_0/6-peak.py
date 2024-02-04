28c0005e2bc5c61cc16f5f2694b891c77f45369c
balagrivine <balagrivine@gmail.com>
2024-02-04 09:42:00 -0800

    try brute force approach

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,12 @@
 def find_peak(list_of_integers):
     """A  function to find a peak in a list of unsorted integer"""
     list_of_integers.sort()
-    if not list_of_integers:
+    """if not list_of_integers:
         return None
     else:
-        max_int = max(list_of_integers)
+        max_int = max(list_of_integers)"""
+    max_int = 0
+    for num in range(list_of_integers):
+        if list_of_integers[num + 1] >= list_of_integers[num]:
+            max_int = list_of_integers[num + 1]
     return max_int
