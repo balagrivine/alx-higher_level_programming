32422a7cdabea3cd9655db7c37639dbe94d744eb
balagrivine <balagrivine@gmail.com>
2024-02-20 05:07:03 -0800

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@
 const request = require('request');
 
 request(process.argv[2], (error, response, body) => {
-	console.log('code: ', response.statusCode);
+	console.log('code:', response.statusCode);
 	return;
 });
