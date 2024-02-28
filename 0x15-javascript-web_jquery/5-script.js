ec18382af2f3a0fb81ee1fdeb77388e357fddbc4
balagrivine <balagrivine@gmail.com>
2024-02-28 04:24:17 -0800

    append a list

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+//JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item:
+
+$(document).ready(function() {
+	$("DIV#add_item").on("click", function() {
+		$("UL>my_list").append("<li>Item</li>");
+	});
+});
